d1d05f6ab2f7049d1b00eaedee1bf14eb9f88398
Emyr298 <emi.ru298@gmail.com>
2024-08-15 10:49:09 +0700

    feat: implement navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import { NavBar } from "@/components/elements/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ChakraProvider>
+          <NavBar />
           {children}
         </ChakraProvider>
       </body>
--- src/components/elements/NavBar/index.tsx
+++ src/components/elements/NavBar/index.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export const NavBar = () => {
   return (
-    <div>
-      
+    <div className="w-full p-3 bg-primary shadow-md">
+      <Link href="/" className="font-bold text-xl">
+        Notes<span className="text-white">App</span>
+      </Link>
     </div>
   );
 };
